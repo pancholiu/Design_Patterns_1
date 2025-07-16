fcff3b4b67a97f6f3b74c5f3509295de34aadf94
Francisco Liu <francisco.gao@mccain.ca>
2025-07-16 18:23:12 -0300

    Memento Pattern - 04 Exercise - Correct Map formation in DocumentState

--- src/MementoExercise/DocumentState.java
+++ src/MementoExercise/DocumentState.java
@@ -7,20 +7,18 @@ public class DocumentState {
     private String content;
     private String fontName;
     private int fontSize;
+    Map<String, Object> values = new HashMap<>();
 
 
     public DocumentState(String content, String fontName, int fontSize) {
         this.content = content;
         this.fontName = fontName;
         this.fontSize = fontSize;
-    }
 
-    public Map<String, Object> getValues() {
-        Map<String, Object> values = new HashMap<>();
         values.put("content", content);
         values.put("fontName", fontName);
         values.put("fontSize", fontSize);
-
-        return values;
     }
+
+    public Map<String, Object> getValues() { return values; }
 }
