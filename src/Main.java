92b1430e88c2829bd8becbf8396aac36e2d8551c
Francisco Liu <pancholiu@hotmail.com>
2025-07-15 22:20:33 -0300

    Memento Pattern - 03 - Implementation

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,25 @@
+import memento.Editor;
+import memento.History;
+
 public class Main {
 
    public static void main(String[] args) {
-       drawUIControl(new Checkbox());
+       var editor = new Editor();
+       var history = new History();
+
+       editor.setContent("a");
+       history.push(editor.createState());
+
+       editor.setContent("b");
+       history.push(editor.createState());
+
+       editor.setContent("c");
+       editor.restore(history.pop());
+       editor.restore(history.pop());
+
+
+       System.out.println(editor.getContent());
    }
 
-   public static void drawUIControl(UIControl control) {
-       control.draw();
-   }
 
 }
