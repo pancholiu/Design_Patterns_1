388b80f22727cc1f9b6db97a0ed185862f1c9ef9
Francisco Liu <pancholiu@hotmail.com>
2025-07-17 17:26:44 -0300

    Memento Pattern - 03 - Task completed (Add  DocumentHistory)

---
+++ src/MementoExercise/DocumentHistory.java
@@ -0,0 +1,18 @@
+package MementoExercise;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DocumentHistory {
+    private List<DocumentState> states = new ArrayList<>();
+
+    public void push(DocumentState state) { states.add(state); }
+
+    public DocumentState pop() {
+        var lastIndex = states.size() - 1;
+        var lastState = states.get(lastIndex);
+        states.remove(lastState);
+
+        return lastState;
+    }
+}
