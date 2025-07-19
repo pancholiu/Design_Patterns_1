5c983fac3066841af82d9517ff17018737c8ad72
Francisco Liu <pancholiu@hotmail.com>
2025-07-18 23:00:12 -0300

    State pattern - 03 - Implementation

--- src/Main.java
+++ src/Main.java
@@ -2,46 +2,18 @@ import memento.Editor;
 import memento.History;
 import mementoExercise.Document;
 import MementoExercise.DocumentHistory;
+import state.Canvas;
+import state.EraserTool;
+import state.SelectionTool;
 
 
 public class Main {
 
    public static void main(String[] args) {
-       var document = new Document();
-       var history = new DocumentHistory();
-
-       document.setContent("la noticia", "Arial", 2);
-       history.push(document.createState());
-
-       document.setContent("la noticia B", "Arial B", 3);
-       history.push(document.createState());
-
-       document.setContent("la noticia C", "Arial C", 4);
-       history.push(document.createState());
-
-       document.restore(history.pop());
-       document.restore(history.pop());
-       document.restore(history.pop());
-
-       System.out.println(document.getContent());
-
-       /*
-       var editor = new Editor();
-       var history = new History();
-
-       editor.setContent("a");
-       history.push(editor.createState());
-
-       editor.setContent("b");
-       history.push(editor.createState());
-
-       editor.setContent("c");
-       editor.restore(history.pop());
-       editor.restore(history.pop());
-
-
-       System.out.println(editor.getContent());
-        */
+      var canvas = new Canvas();
+      canvas.setCurrentTool(new EraserTool());
+      canvas.mouseDown();
+      canvas.mouseUp();
    }
 
 
