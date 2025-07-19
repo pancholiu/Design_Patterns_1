b84a4e0fb87577787a37bf06d3c6d96369db0ba9
Francisco Liu <pancholiu@hotmail.com>
2025-07-19 20:14:20 -0300

    Iterator Pattern - 01 - The Problem

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
+import iterator.BrowseHistory;
 import state.Exercise.*;
 
 
 public class Main {
 
    public static void main(String[] args) {
-      var directionService = new DirectionService(new DrivingMode());
+      var history = new BrowseHistory();
+      history.push("a");
+      history.push("b");
+      history.push("c");
 
-      directionService.getETA();
-      directionService.getDirection();
+      for(var i = 0; i < history.getUrls().size(); i++) {
+         var url = history.getUrls().get(i);
+         System.out.println(url);
+      }
    }
 
 
---
+++ src/iterator/BrowseHistory.java
@@ -0,0 +1,24 @@
+package iterator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BrowseHistory {
+    private String[] urls = new String[10];
+
+    public void push(String url) {
+        urls.add(url);
+    }
+
+    public String pop() {
+        var lastIndex = urls.size() - 1;
+        var lastUrl = urls.get(lastIndex);
+        urls.remove(lastUrl);
+
+        return lastUrl;
+    }
+
+    public String[] getUrls() {
+        return urls;
+    }
+}
