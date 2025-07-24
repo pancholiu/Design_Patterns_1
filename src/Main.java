af42774d690f12423acfaace9db921570066457e
Francisco Liu <pancholiu@hotmail.com>
2025-07-24 19:54:40 -0300

    Strategy Pattern - 01 - The Problem

--- src/Main.java
+++ src/Main.java
@@ -9,17 +9,6 @@ public class Main {
 
    public static void main(String[] args) {
 
-      var collection = new ProductCollection();
-      collection.add(new Product(1, "a"));
-      collection.add(new Product(2, "b"));
-      collection.add(new Product(3, "c"));
-
-      IteradorSP collectionIterator = collection.createIterator();
-
-      while(collectionIterator.hasNext()) {
-         var product = collectionIterator.current();
-         System.out.println(product);
-         collectionIterator.next();
       }
    }
 }
