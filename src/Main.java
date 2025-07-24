a6478b47e85e16e67ef25c3768e69768fd97954b
Francisco Liu <pancholiu@hotmail.com>
2025-07-24 20:12:01 -0300

    Strategy Pattern - 03 - Implementation

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,23 @@ import iterator.IteradorSp;
 import iterator.excercise.IteradorSP;
 import iterator.excercise.Product;
 import iterator.excercise.ProductCollection;
+import strategy.BlackAndWhiteFilter;
+import strategy.ImageStorage;
+import strategy.JpegCompressor;
+import strategy.PngCompressor;
 
 
 public class Main {
 
    public static void main(String[] args) {
+         var imageStorage = new ImageStorage();
 
-      }
+         imageStorage.store("a",
+                 new PngCompressor(),
+                 new BlackAndWhiteFilter());
+
+         imageStorage.store("a",
+                 new JpegCompressor(),
+                 new BlackAndWhiteFilter());
    }
 }
--- src/strategy/ImageStorage.java
+++ src/strategy/ImageStorage.java
@@ -1,23 +1,9 @@
 package strategy;
 
 public class ImageStorage {
-    private String compressor;
-    private String filter;
 
-    public ImageStorage(String compressor, String filter) {
-        this.compressor = compressor;
-        this.filter = filter;
-    }
-
-    public void store(String fileName) {
-        if(compressor == "jpeg")
-            System.out.println("Compressing using JPEG");
-        else if(compressor == "png")
-            System.out.println("Compressing using PNG");
-
-        if(filter == "b&w")
-            System.out.println("Applying B&W filter");
-        else if(filter == "high-contrast")
-            System.out.println("Applying high-contrast contrast filter");
+    public void store(String fileName, Compressor compressor, Filter filter) {
+        compressor.compress(fileName);
+        filter.apply(fileName);
     }
 }
