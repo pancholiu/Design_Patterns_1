4b65089ec7561a8bc726c771301728c3c9bf9581
Francisco Liu <pancholiu@hotmail.com>
2025-07-27 21:00:14 -0400

    Template Pattern - 01 - The Problem

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,7 @@ import strategy.excercise.DesEncyption;
 public class Main {
 
    public static void main(String[] args) {
-        var chatClient = new ChatClient();
-
-        chatClient.send("The message", new DesEncyption());
+        // Transfer money
+       // Generate report
    }
 }
---
+++ src/template/GenerateReportTask.java
@@ -0,0 +1,15 @@
+package template;
+
+public class GenerateReportTask {
+    private AuditTrail auditTrail;
+
+    public GenerateReportTask(AuditTrail auditTrail) {
+        this.auditTrail = auditTrail;
+    }
+
+    public void execute() {
+        auditTrail.record();
+
+        System.out.println("Generate report");
+    }
+}
