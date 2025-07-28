99cc2528c667c8288801fdd18333a60314f62b7b
Francisco Liu <francisco.gao@mccain.ca>
2025-07-28 17:58:31 -0400

    Template Pattern - 03 - Implementation

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 import strategy.excercise.AesEncryption;
 import strategy.excercise.ChatClient;
 import strategy.excercise.DesEncyption;
+import template.TransferMoneyTask;
 
 public class Main {
-
    public static void main(String[] args) {
-        // Transfer money
-       // Generate report
+      var task = new TransferMoneyTask();
+      task.execute();
    }
 }
--- src/template/TransferMoneyTask.java
+++ src/template/TransferMoneyTask.java
@@ -1,15 +1,8 @@
 package template;
 
-public class TransferMoneyTask {
-    private AuditTrail auditTrail;
-
-    public TransferMoneyTask(AuditTrail auditTrail) {
-        this.auditTrail = auditTrail;
-    }
-
-    public void execute() {
-        auditTrail.record();
-
-        System.out.println("Transfer Money");
+public class TransferMoneyTask extends Task {
+    @Override
+    protected void doExecute() {
+        System.out.println("Transfer money");
     }
 }
