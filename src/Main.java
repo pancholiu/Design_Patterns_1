9d178a1db4482c5be0e85f76b9e8ca4d50ce5b80
Francisco Liu <francisco.gao@mccain.ca>
2025-07-30 15:33:05 -0400

    Command Pattern - 06 - Implementing Undo Mechanism

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,22 @@
 import command.*;
+import command.editor.BoldCommand;
+import command.editor.History;
+import command.editor.HtmlDocument;
+import command.editor.UndoCommand;
 import command.fx.Button;
 
 public class Main {
    public static void main(String[] args) {
-      var composite = new CompositeCommand();
-      composite.add(new ResizeCommand());
-      composite.add(new BlackAndWhiteCommand());
-      composite.execute();
+      var history = new History();
+      var document = new HtmlDocument();
+      document.setContent("Hello world");
+
+      var boldCommand = new BoldCommand(document, history);
+      boldCommand.execute();
+      System.out.println(document.getContent());
+
+      var undoCommand = new UndoCommand(history);
+      undoCommand.execute();
+      System.out.println(document.getContent());
    }
 }
