706f4ddf9e1518f6a04020c6e122855b2419b9ff
Francisco Liu <francisco.gao@mccain.ca>
2025-08-01 11:41:50 -0400

    Observer Pattern - 05 - The Pull Style

--- src/observer/Chart.java
+++ src/observer/Chart.java
@@ -1,8 +1,14 @@
 package observer;
 
 public class Chart implements Observer {
+    private DataSource dataSource;
+
+    public Chart(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Override
     public void update(int value) {
-        System.out.println("Chart got updated: " + value);
+        System.out.println("Chart got updated: " + dataSource.getValue());
     }
 }
