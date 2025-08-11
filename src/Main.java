46ce819593366a835a7d31cda4ab4d8d7aad3110
Francisco Liu <francisco.gao@mccain.ca>
2025-08-11 16:40:15 -0400

    Mediator Pattern - 03 - Implementation

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,10 @@
-import observer.Chart;
-import observer.DataSource;
-import observer.SpreadSheet;
-import observer.excercise.StatusBar;
-import observer.excercise.Stock;
-import observer.excercise.StockListView;
+import mediator.ArticlesDialogBox;
 
 public class Main {
    public static void main(String[] args) {
-      var stock = new Stock();
+      var dialog = new ArticlesDialogBox();
 
-      var statusBar = new StatusBar(stock);
-      var stockListView = new StockListView(stock);
+      dialog.simulateUserInteraction();
 
-      stock.addObserver(statusBar);
-      stock.addObserver(stockListView);
-
-      stock.setPrice(7);
-      stock.setSymbol("Hola");
-
-      stock.setPrice(8);
-      stock.setSymbol("Mundo");
    }
 }
